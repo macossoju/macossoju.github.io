7e1b8d438006dcad7af54ba7a91b78b60588d7f6
macossoju <134232652+macossoju@users.noreply.github.com>
2023-06-17 15:11:04 +0300

    Delete photoshow directory

--- photoshow/characters-fly-out-typing/README.md
+++
@@ -1,4 +0,0 @@
-# Animating typed chars
-
-A Pen created on CodePen.io. Original URL: [https://codepen.io/multum/pen/BZaPMe](https://codepen.io/multum/pen/BZaPMe).
-
--- photoshow/characters-fly-out-typing/dist/script.js
+++
@@ -1,7 +0,0 @@
-$( document ).ready( function () {
-    $( "input" ).airChars( {
-        duration: 1,
-        upperLimit: 200,
-        sizeInterval: [ 15, 80 ]
-    } );
-} );
